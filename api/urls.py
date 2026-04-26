026cd41016022ec3da8c58e9b3157f223e080f7d
annapmarin <annapratsmarin@gmail.com>
2026-04-26 18:13:40 +0200

    feat: add private endpoints for creating artists and events

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, EventListView, EventDetailView
+from .views import RegisterView, LoginView, EventListView, EventDetailView, CreateArtistView, CreateEventView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('events/', EventListView.as_view(), name='event-list'),
     path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
+    path('artists/', CreateArtistView.as_view(), name='create-artist'),
+    path('events/create/', CreateEventView.as_view(), name='create-event'),
 ]
