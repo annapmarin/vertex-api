623e852485ba5d0c2722bfdf99dde600f650e251
annapmarin <annapratsmarin@gmail.com>
2026-04-26 14:19:34 +0200

    feat: update urls.py with public endpoints urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, EventListView, EventDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('events/', EventListView.as_view(), name='event-list'),
+    path('events/<int:pk>/', EventDetailView.as_view(), name='event-detail'),
 ]
