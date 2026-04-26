3d0759e277ade85d600ff649ce9f888248daceb7
annapmarin <annapratsmarin@gmail.com>
2026-04-26 18:28:26 +0200

    feat: welcome page

--- vertex/urls.py
+++ vertex/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import WelcomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path("", WelcomeView.as_view(), name="welcome"),
 ]
